aba75c501597962ed7c9b1108c16dbb0e7e435ba
j <settieyeah@gmail.com>
2018-04-26 16:42:04 -0300

    Add files via upload

---
+++ processoSO1.c
@@ -0,0 +1,34 @@
+
+#include <linux/unistd.h>
+#include <linux/linkage.h>
+#include <linux/sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+asmlinkage long sys_callordenar(pid_t pid) {
+
+	int i;
+	int vetor[10];
+	int status = 0;
+
+	for (i = 0; i < 10; i++){
+		pidteste = fork();
+
+		if (pidteste == 0) {
+			vetor[i] = getpid();
+			exit(0);
+		}
+
+		wait(&status); 
+	}
+
+	for (i = 0; i < 10; i++){
+		if (vetor[i]%2 == 0)
+			kill(vetor[i], SIGKILL);
+	}
+
+}
+
+
